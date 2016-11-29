67d7ef5ff7b8f3665a66d4b90c110bd4a2dcc3b4
quinlan-alys <shannonqad@gmail.com>
2016-11-29 10:25:48 -0800

    adding variables

--- themes/yell/gulpfile.js
+++ themes/yell/gulpfile.js
@@ -63,7 +63,7 @@ gulp.task('browser-sync', function() {
    ];
 
     browserSync.init(files, {
-        proxy: 'inhabitent.dev',
+        proxy: 'localhost:8888/yell/',
     });
 
     gulp.watch(files).on('change', browserSync.reload);
