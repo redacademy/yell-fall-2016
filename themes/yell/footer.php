ca2ec681b0fe2298d57ab46f50db0840cfa83790
Kiana Aghaei hashtjin <kiana@Kianas-MacBook-Air.local>
2016-11-29 15:01:05 -0800

    footer

--- themes/yell/footer.php
+++ themes/yell/footer.php
@@ -1,22 +1,40 @@
 <?php
 /**
- * The template for displaying the footer.
- *
- * @package RED_Starter_Theme
- */
-
+* The template for displaying the footer.
+*
+* @package RED_Starter_Theme
+*/
 ?>
+	</div>
+	<!-- #content -->
+	<!--Site Footer-->
+	<div class="site-info">
+		<footer id="colophon" class="site-footer" role="contentinfo">
+			<div class="footer-nav-left">
+				<ul class="nav-bar">
+					<li class="nav-list"><a href="#Home">Home</a></li>
+					<li class="nav-list"><a href="#About Us">About Us</a></li>
+					<li class="nav-list"><a href="#Program Details">Program Details</a></li>
+					<li class="nav-list"><a href="#Info for Students">Info for Students</a></li>
+				</ul>
+			</div>
+			<div class="footer-nav-right">
+				<ul class="nav-bar">
+					<li class="nav-list"><a href="#Be an Expert">Be an Expert</a></li>
+					<li class="nav-list"><a href="#Become a Partner">Become a Partner</a></li>
+					<li class="nav-list"><a href="#Press and Stories">Press and Stories</a></li>
+					<li class="nav-list"><a href="#Info for Students">Info for Students</a></li>
+				</ul>
+			</div>
+			
+			<div class="copyright">
+				<p>Copyright © 2016 Yell</p>
+			</div>
 
-			</div><!-- #content -->
-
-			<footer id="colophon" class="site-footer" role="contentinfo">
-				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
-				</div><!-- .site-info -->
-			</footer><!-- #colophon -->
-		</div><!-- #page -->
-
-		<?php wp_footer(); ?>
-
-	</body>
-</html>
+	</div>
+	<!-- .site-info -->
+	</footer>
+	<!-- #colophon -->
+	</div>
+	<!-- #page -->
+	<?php wp_footer(); ?>
