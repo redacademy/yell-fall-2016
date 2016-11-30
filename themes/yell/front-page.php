1a47d0d6b1559613a1a7730922ae63185819e73d
quinlan-alys <shannonqad@gmail.com>
2016-11-29 20:02:18 -0800

    adding custom fields to home page

--- themes/yell/front-page.php
+++ themes/yell/front-page.php
@@ -1,6 +1,5 @@
 <?php
-/**
- * The template for displaying all pages.
+/** Template Name: Front Page 
  *
  * @package RED_Starter_Theme
  */
@@ -10,23 +9,32 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-			<?php while ( have_posts() ) : the_post(); ?>
+<?php
+    $props = CFS()->get_field_info( 'what_yell' );
+    echo '<h2>' . $props['label'] . '</h2>';
+    ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+    <?php echo CFS()->get( 'what_yell' ); ?>
+		
+		 <button class="more-about"> More About Yell ></button>
+		 
+	<?php
+    $props = CFS()->get_field_info( 'impact' );
+    echo '<h2>' . $props['label'] . '</h2>';
+    ?>
+		<div class="impact">
+    <?php echo CFS()->get( 'impact' ); ?>
+		</div>
 
-			<?php endwhile; // End of the loop. ?>
-				<h2> What is YELL</h2>
-             
-                <button class="more-about"> More About Yell ></button>
-
-				<h2> Impact </h2>
-				<h4> Student Impact</h4>
-
-				 <button class="more-about"> More Results ></button>
-			<div class="testimonial">
-				<div class="student-name">
+	<div class="testimonial-wrapper">
+		<div class="testimonial">
+   		 <?php echo CFS()->get( 'testimonial' ); ?>
+		</div>
+			<div class="student-name">
+				<?php echo CFS()->get('student_name'); ?>
 					</div>
 			</div>
+
 				  <button class="get-involved"> Get Involved! </button>
 		</main><!-- #main -->
 	</div><!-- #primary -->
