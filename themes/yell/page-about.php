6a2251b3f49bf85c00a6c7e71108cc99c884973f
Kiana Aghaei <kiana.aghaei@gmail.com>
2016-12-01 11:52:17 -0800

    working on about page

--- themes/yell/page-about.php
+++ themes/yell/page-about.php
@@ -8,17 +8,21 @@
  */
 
 get_header(); ?>
-
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-<?php get_footer(); ?>
-
+			<div class="homepg-bg1">
+				<div class="homepg-bg2">
+					<div class="homepg-bg3">
+						<?php echo CFS()->get( 'Want to learn more?' ); ?>
+						<div class="aboutpage-button">
+						<button class="contact-us"> Contact Us ></button>
+						<button class="programs"> Programs ></button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+		<!-- #main -->
+	</div>
+	<!-- #primary -->
+	<?php get_footer(); ?>
