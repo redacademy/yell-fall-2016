fab76125c5f6860e1b8e659a58a087655f1d205b
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-30 22:29:13 -0800

    Deleted comments from yell js file.:

--- themes/yell/js/yell.js
+++ themes/yell/js/yell.js
@@ -1,11 +1,3 @@
-/*( function( $ ) {
-
-	$('.menu-toggle').on('click', function(event) {
-		event.preventDefault();
-		$('#primary-menu ul').toggle("slide", {direction: "left"}, 2000);
-})
-})( jQuery );
-*/
 ( function( $ ) {
 
 	$('.menu-toggle').on('click', function(event) {
