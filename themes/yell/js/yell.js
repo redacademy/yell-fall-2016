3775c649ed124f638ac83187961300aab630c2bc
Djordje Vujatovic <djordje_v@hotmail.com>
2016-11-30 22:27:43 -0800

    Fixed toggle on nav bar. Toggles in and out.

--- themes/yell/js/yell.js
+++ themes/yell/js/yell.js
@@ -1,8 +1,18 @@
-( function( $ ) {
+/*( function( $ ) {
 
 	$('.menu-toggle').on('click', function(event) {
-		console.log('hi htere');
 		event.preventDefault();
 		$('#primary-menu ul').toggle("slide", {direction: "left"}, 2000);
 })
+})( jQuery );
+*/
+( function( $ ) {
+
+	$('.menu-toggle').on('click', function(event) {
+		event.preventDefault();
+		$('#primary-menu ul').animate({width: 'toggle'}).focus();
+})
+	$('#primary-menu ul').focusout(function(){
+		$('#primary-menu ul').animate({width:toggle});
+	})
 })( jQuery );
