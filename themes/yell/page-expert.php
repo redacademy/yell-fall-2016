5800e1c01d39c3adcc8e97690593498b1c7840de
quinlan-alys <shannonqad@gmail.com>
2016-12-01 21:48:16 -0800

    styling desktop version of experts pg

--- themes/yell/page-expert.php
+++ themes/yell/page-expert.php
@@ -23,7 +23,7 @@ get_header(); ?>
 
 					
 
-		
+			<div class="testimonial-flex">
 					<div class="testimonial-wrapper">
 						<div class="testimonial">
 						<?php $fields=CFS()->get( 'testimonial' );
@@ -34,43 +34,64 @@ get_header(); ?>
 										</div>
 
 					</div>
-
-											<p><?php echo CFS()->get( 'expert_paragraph' ); ?></p>
-						<div class="speaker-btn"> 
-						Guest Speakers at YELL
-						</div>
-						<div class="speaker-info">
-						<?php echo CFS()->get( 'guest_speakers_at_yell' ); ?>
-						<button class="get-involved"> Be a guest speaker! </button>
-						</div> 
-						<div class="mentor-btn"> 
-						Mentors at YELL
-						</div>
-						<div class="mentor-info">
-						
-						<p> 
-
-								We believe that mentoring with YELL will be a positive and enriching experience for you. Specifically, we hope you will:</p>
-								<ul class="mentor-ul">
-								<li> Refine your coaching and mentoring skills</li>
-								<li>Develop meaningful connections with students</li>
-								<li>Support the development of future leaders in your community </li>
-								<li>Learn about the YELL organization and the next generation of entrepreneurs </li>
-								<li> Expand your professional network </li>
-								</ul>
-								<p>
-								As a mentor you play several roles, including:</p>
-							<div class="mentor">
-								<?php $fields=CFS()->get( 'mentors' );
+						<div class="testimonial-wrapper2">
+						<div class="testimonial">
+						<?php $fields=CFS()->get( 'testimonial' );
 							foreach ( $fields as $field ) { ?>
-							
-									
-										<h5><?php echo $field['mentor_type']; ?></h5>
-									<p><?php echo $field['mentor_description']; } ?></p>
-									</div>
-									<button class="get-involved"> Be a YELL Mentor! </button>
+								  <?php echo '<img src="' . $field['testimonial_image'] . '" class="testimonial-img">';  ?>
+									<p><?php echo $field['testimonial_text'];  ?></p></div>
+										<div class="student-name"><?php echo $field['testimonial_name']; } ?>
+										</div>
+
+					</div>
+						<div class="testimonial-wrapper3">
+						<div class="testimonial">
+						<?php $fields=CFS()->get( 'testimonial' );
+							foreach ( $fields as $field ) { ?>
+								  <?php echo '<img src="' . $field['testimonial_image'] . '" class="testimonial-img">';  ?>
+									<p><?php echo $field['testimonial_text'];  ?></p></div>
+										<div class="student-name"><?php echo $field['testimonial_name']; } ?>
+										</div>
+
+					</div>
+				</div>
+						<div class="expert-info"><?php echo CFS()->get( 'expert_paragraph' ); ?></div>
+						
+						
+						
+		<div class="expert-wrapper">
+
+			<div class="speaker-btn">Guest Speakers at YELL
+			</div>
+				<div class="speaker-info">
+					<div class="speaker-infom">	
+                        <h2>Guest Speakers at YELL<h2>
 					</div>
 
+						<?php echo CFS()->get( 'guest_speakers_at_yell' ); ?>
+						<button class="get-involved"> Be a guest speaker! </button>
+							
+				</div> 
+			
+
+				<div class="mentor-btn"> 
+						Mentors at YELL
+				</div>
+					<div class="mentor-info">
+							<h2> Mentors at YELL</h2>
+							<img src="<?php echo get_template_directory_uri(); ?>/images/mentor.png" alt= "mentor chart" class="mentor-chart"> 
+							<p> We believe that mentoring with YELL will be a positive and enriching experience for you. Specifically, we hope you will:</p>
+								<ul class="mentor-ul">
+									<li> Refine your coaching and mentoring skills</li>
+									<li>Develop meaningful connections with students</li>
+									<li>Support the development of future leaders in your community </li>
+									<li>Learn about the YELL organization and the next generation of entrepreneurs </li>
+									<li> Expand your professional network </li>
+								</ul>
+								<button class="get-involved"> Be a YELL Mentor! </button>
+					</div>
+
+			</div>
 
 				<div class="teacher-btn"> 
 						Details For Teachers
@@ -89,12 +110,13 @@ get_header(); ?>
 				</div>
 		
 			<h2> Program Structure</h2>
-					<div class="program">
+		<div class="expert-wrapper">
+				<div class="program">
 							<img src="<?php echo get_template_directory_uri(); ?>/images/guage.png" alt= "accelerator">  
 				<h3> Business Accelerator</h3>
 				<h5> Fall Semester</h5>
 				<?php echo CFS()->get( 'fall' ); ?>
-					</div>
+				</div>
 					
 					<div class="program">
 							<img src="<?php echo get_template_directory_uri(); ?>/images/idea-incubator.png" alt= "brain">  
@@ -108,8 +130,9 @@ get_header(); ?>
 					<h3> Venture Challenge</h3>
 					<h5> Spring</h5>
 				<?php echo CFS()->get( 'spring' ); ?>
-				<button class="get-involved"> YELL at your school! </button>
+		</div>
 					</div>
+					<button class="get-involved"> YELL at your school! </button>
 </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
