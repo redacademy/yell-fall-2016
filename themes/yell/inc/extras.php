42c17fdb40af909246b689e1b461c6822fad6d68
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-02 14:06:50 -0800

    Added professionals hero image.

--- themes/yell/inc/extras.php
+++ themes/yell/inc/extras.php
@@ -67,6 +67,17 @@ function my_styles_method() {
        }";
 
        wp_add_inline_style( 'red-starter-style', $custom_css );
+ }elseif(is_page( 'professionals' )){
+
+       $url = CFS()->get( 'professionals_background_image' );//This is grabbing the background image vis Custom Field Suite Plugin
+       $custom_css = "
+       .expert-hero{
+         background: url({$url}) no-repeat center bottom;
+         height:100vh;
+         background-size: cover, cover;
+       }";
+
+       wp_add_inline_style( 'red-starter-style', $custom_css );
 }}
      add_action( 'wp_enqueue_scripts', 'my_styles_method' );
 
