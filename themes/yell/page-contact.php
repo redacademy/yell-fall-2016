3dd0f7b6d1deebe2204335e5065c6ac607710c2b
quinlan-alys <shannonqad@gmail.com>
2016-12-02 16:21:05 -0800

    contact page info and styling

--- themes/yell/page-contact.php
+++ themes/yell/page-contact.php
@@ -19,6 +19,24 @@ get_header(); ?>
 
 			<?php endwhile; // End of the loop. ?>
 
+			<h3>Contact Us</h3>
+			<div class="contactinfo">
+			<div class="child1">
+				<p> Are you a professional who wants to take 
+				part of YELL?</p>
+			<button class="get-involved">Professionals</button>
+			</div>
+			<div class="child2">
+			<p> Are you a potential sponsor, business
+ 				or a community partner?</p>
+			<button class="get-involved">Supporters</button>
+			</div>
+			<div class="child3">
+			<p> Have questions? Send us a message.</p>
+			<button class="get-involved2">General Inquiries</button>
+			</div>
+
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 <?php get_footer(); ?>
