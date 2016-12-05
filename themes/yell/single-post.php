9d31900d51244cb7472e651de4ad72afa1bc7840
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-04 18:20:41 -0800

    Edited nav-bar, added single-post page.

---
+++ themes/yell/single-post.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * The template for displaying all single posts.
+ *
+ * @package RED_Starter_Theme
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+<div class="single-post-section">
+<div class='single-post-wrapper'>
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'template-parts/content', 'single' ); ?>
+
+
+		<?php endwhile; // End of the loop. ?>
+</div>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+<div>
+<?php get_footer(); ?>
