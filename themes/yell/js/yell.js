881b121f260ffdf6faf0b23facad5a7533dbf577
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-06 14:16:57 -0800

    Fixed site nav-bar. Changed to primary menu on backend.

--- themes/yell/js/yell.js
+++ themes/yell/js/yell.js
@@ -2,7 +2,7 @@
 
 	$('.menu-toggle').on('click', function(event) {
 		event.preventDefault();
-		$('#primary-menu ul').animate({width: 'toggle'}).focus();
+		$('.main-navigation ul').animate({width: 'toggle'}).focus();
 		$('.main-navigation ul').css("background-color","#e8e8e8");
 		
 
