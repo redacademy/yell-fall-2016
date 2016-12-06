31c8bafe06b4c5b65a9b81816f30d8aa29236e9a
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-06 11:55:40 -0800

    Added styling to thank you page, mobile.

--- themes/yell/page-thankyou.php
+++ themes/yell/page-thankyou.php
@@ -11,14 +11,15 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-             <div class="thanks-hero"> <h1>Thank you for Getting Involved!</h1>
+             <div class="thanks-hero">
+				<div class="header-container">
+				  <h1>Thank you for Getting Involved!</h1>
+				</div>
 			</div>	
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+		 <div class="thanks-body">
+			 <h1>We'll get back to you shortly!</h1>
+			<button class="back-home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"> Back Home </a></button>
+		</div>
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
