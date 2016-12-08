783f4188b4added4ca1bf9a0e4438bab2c444392
quinlan-alys <shannonqad@gmail.com>
2016-12-08 14:21:16 -0800

    deleting progress bars from image file

--- themes/yell/page-forms.php
+++ themes/yell/page-forms.php
@@ -12,15 +12,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-             <div class="progress-bar">
-			  <img src="<?php echo get_template_directory_uri(); ?>/images/progress1.png" alt= "Progress Bar 1"> 
-              </div>
-              <div class="progress-bar2">
-              <img src="<?php echo get_template_directory_uri(); ?>/images/progress2.png" alt= "Progress Bar 2"> 
-              </div>
-              <div class="progress-bar3">
-              <img src="<?php echo get_template_directory_uri(); ?>/images/progress3.png" alt= "Progress Bar 3"> 
-			</div>
+            
 
 			<?php while ( have_posts() ) : the_post(); ?>
 
