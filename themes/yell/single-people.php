7d72ac3b4dcb37a28f8f4a992bfcac27907de06b
Kiana Aghaei <kiana.aghaei@gmail.com>
2016-12-09 08:20:08 -0800

    single post types (people)

--- themes/yell/single-people.php
+++ themes/yell/single-people.php
@@ -10,23 +10,17 @@ get_header(); ?>
 			<?php while ( have_posts() ) : the_post(); ?>
 			<section class="single-people-container">
 				<div class="single-people-thumb">
-					<?php the_post_thumbnail( 'category-thumb'); ?>
+					<?php the_post_thumbnail(); ?>
 				</div>
 				<div class="single-people-content">
 					<h2 class="single-people-title">
 						<?php the_title();?>
 					</h2>
-					<p class="single-people-price">
-						<?php echo CFS()->get( 'people_price' ); ?>
-					</p>
+				
 					<p class="single-people-description">
 						<?php the_content();?>
 					</p>
-					<div class="social-media-button">
-						<a class="like-button" href="#"><i class="fa fa-facebook" aria-hidden="true"></i> Like</a>
-						<a class="tweet-button" href="#"><i class="fa fa-twitter" aria-hidden="true"></i> Tweet</a>
-						<a class="pin-button" href="#"><i class="fa fa-pinterest" aria-hidden="true"></i> Pin</a>
-					</div>
+				
 				</div>
 			</section>
 			<?php
