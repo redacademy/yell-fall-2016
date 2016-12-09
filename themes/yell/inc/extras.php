832d2d32c54cc3151f26b42a2221a0526478b1aa
quinlan-alys <shannonqad@gmail.com>
2016-12-08 16:09:59 -0800

    deleting readmore btn

--- themes/yell/inc/extras.php
+++ themes/yell/inc/extras.php
@@ -22,14 +22,6 @@ function red_starter_body_classes( $classes ) {
 add_filter( 'body_class', 'red_starter_body_classes' );
 
 
-//////////////// add a readmore buttom to posts///////////////////
-
-// function new_excerpt_more($more) {
-//        global $post;
-// 	return '<div class="readmore"><a class="moretag" href="'. get_permalink($post->ID) . '"> read more > </a></div>';
-// }
-// add_filter('excerpt_more', 'new_excerpt_more');
-
 
 /////////////Custom About Page background Image (hero banner)////////////
 function my_styles_method() {
--- themes/yell/page-get-involved.php
+++ themes/yell/page-get-involved.php
@@ -24,7 +24,7 @@ get_header(); ?>
 </div>
 
 <div class="btn-wrapper">
-	<a href="<?php echo esc_url( site_url( '/get_involved/' ) ); ?>" rel="get involved"><div class="studentbtn"><h5>Student ></h5><p> Become a future student at YELL</p></div></a>
+	<a href="<?php echo esc_url( site_url( '/get-involved/' ) ); ?>" rel="get involved"><div class="studentbtn"><h5>Student ></h5><p> Become a future student at YELL</p></div></a>
 	<a href="<?php echo esc_url( site_url( '/guest-speaker-form/' ) ); ?>" rel="guest form"><div class="expertbtn"><h5>Professional ></h5><p> You are an entrepeneur or a guest speaker</p></div></a>
 	<a href="<?php echo esc_url( site_url( '/support/' ) ); ?>" rel="support"><div class="supportbtn"><h5>Supporter ></h5><p> Help support or fund  YELL</p></div></a>
 </div>
