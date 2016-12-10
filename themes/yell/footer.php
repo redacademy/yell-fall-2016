693b9b382fa988a1b7d779b9c325e753d7aa4733
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-10 14:23:56 -0800

    Fixed footer: Cleaned code. Fixed bugs.

--- themes/yell/footer.php
+++ themes/yell/footer.php
@@ -7,46 +7,36 @@
 ?>
 	<!-- #content -->
 	<!--Site Footer-->
-	<div class="site-info">
-		<footer id="colophon" class="site-footer" role="contentinfo">
-			<div class="icons-block">
-			<div class="footer-button">
-			<a href="<?php echo esc_url( site_url( '/get-involved/' ) ); ?>" rel="get involved">	<button class="get-involved-footer"> Get Involved </button></a>
-			</div>
-			<div class="icons">
-				<i class="fa fa-facebook-square" aria-hidden="true"></i>
-				<i class="fa fa-twitter" aria-hidden="true"></i>
-				<i class="fa fa-instagram" aria-hidden="true"></i>
-			</div>
-			<div class="copyright-desktop">
-				<span class="copy">Copyright © 2016 YOUNG ENTREPRENEUR LEADERSHIP LAUNCHPAD</span>
-			</div>
-			</div>
-			<div class="footer-content">
-			<div class="footer-nav-left">
-				<ul class="footer-nav-bar">
-					<li class="nav-list"><a href="#Home">Home</a></li>
-					<li class="nav-list"><a href="#About Us">About Us</a></li>
-					<li class="nav-list"><a href="#Program Details">Program Details</a></li>
-					<li class="nav-list"><a href="#Info for Students">Info for Students</a></li>
-				</ul>
-			</div>
-			<div class="footer-nav-right">
-				<ul class="footer-nav-bar">
-					<li class="nav-list"><a href="#Be an Expert">Be an Expert</a></li>
-					<li class="nav-list"><a href="#Become a Partner">Become a Partner</a></li>
-					<li class="nav-list"><a href="#Press and Stories">Press and Stories</a></li>
-					<li class="nav-list"><a href="#Contact Us">Contact Us</a></li>
-				</ul>
-			<div class="copyright">
-				<span class="copy">Copyright © 2016 COPYRIGHT YOUNG ENTREPRENEUR LEADERSHIP LAUNCHPAD</span>
-			</div>
-			</div>
-			</div>
-			
-	<!-- .site-info -->
-	</footer>
-	<!-- #colophon -->
+<section class="footer-container">
+	<div class="hi">
+	<div class ="get-involved-button-footer">
+		<button class="get-involved"><a href="<?php echo esc_url( site_url( '/get-involved' ) ); ?>" rel="get involved"> Get Involved!</a></button>
 	</div>
+	<div class="social-media-icons">
+		 <i class="fa fa-facebook-square" aria-hidden="true"></i>
+         <i class="fa fa-twitter" aria-hidden="true"></i>
+         <i class="fa fa-instagram" aria-hidden="true"></i>
+	</div>
+	</div>
+    <div class="colophon-footer">
+	<div class="colophon-left">
+		<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><p>Home</p></a>
+		<a href="<?php echo esc_url( home_url( '/about' ) ); ?>" rel="about"><p>About Us</p></a>
+		<a href="<?php echo esc_url( home_url( '/programs' ) ); ?>" rel="programs"><p>Program Details</p></a>
+		<a href="<?php echo esc_url( home_url( '/students' ) ); ?>" rel="student info"><p>Info for Students</p></a>
+    </div>
+	<div class="colophon-right">
+		<a href="<?php echo esc_url( home_url( '/professionals' ) ); ?>" rel="professionals"><p>Be a Professional</p></a>
+		<a href="<?php echo esc_url( home_url( '/supporters' ) ); ?>" rel="supporters"><p>Become a Partner</p></a>
+		<a href="<?php echo esc_url( home_url( '/press-stories-2' ) ); ?>" rel="press and stories"><p>Press and Stories</p></a>
+		<a href="<?php echo esc_url( home_url( '/contact' ) ); ?>" rel="contact"><p>Contact Us</p></a>	
+	</div>
+	</div>
+	<div class ="site-copyright">
+		<p>Young Entrepreneur Leadership Launchpad</p>
+	</div>
+</section>
+
+</section>
 	<!-- #page -->
 	<?php wp_footer(); ?>
