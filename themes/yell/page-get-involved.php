019ee5f80f522e7a13fa11622cb5135978de9be1
Djordje Vujatovic <djordje_v@hotmail.com>
2016-12-09 22:58:37 -0800

    Fixed get-involved page

--- themes/yell/page-get-involved.php
+++ themes/yell/page-get-involved.php
@@ -12,21 +12,16 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
-			<?php while ( have_posts() ) : the_post(); ?>
-
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
-
-			<?php endwhile; // End of the loop. ?>
+<section class ="get-involved-title">		
 <div class="involved-info">
 			<h3> Get Involved </h3>
 			<p> Who are you? </p>
 </div>
-
+</section>
 <div class="btn-wrapper">
-	<div class="studentbtn"><a href="<?php echo esc_url( site_url( '/get-involved/' ) ); ?>" rel="get involved"><h5>Student <span>></span></h5><p> Become a future student at YELL</p></a></div>
-	<div class="expertbtn"><a href="<?php echo esc_url( site_url( '/guest-speaker-form/' ) ); ?>" rel="guest form"><h5>Professional <span>></span></h5><p> You are an entrepeneur or a guest speaker</p></a></div>
-	<div class="supportbtn"><a href="<?php echo esc_url( site_url( '/support/' ) ); ?>" rel="support"><h5>Supporter<span> ></span></h5><p> Help support or fund  YELL</p></a></div>
+	<div class="studentbtn"><a href="<?php echo esc_url( site_url( '/get-involved/' ) ); ?>" rel="get involved"><span class="words-container"><h5>Student <span class="arrow">></span></h5><p> Become a future student at YELL</p></span></a></div>
+	<div class="expertbtn"><a href="<?php echo esc_url( site_url( '/guest-speaker-form/' ) ); ?>" rel="guest form"><span class="words-container"><h5>Professional <span>></span></h5><p> You are an entrepeneur or a guest speaker</p></span></a></div>
+	<div class="supportbtn"><a href="<?php echo esc_url( site_url( '/support/' ) ); ?>" rel="support"><span class="words-container"><h5>Supporter <span>><span></h5><p> Help support or fund  YELL</p></span></a></div>
 </div>
 		</main><!-- #main -->
 	</div><!-- #primary -->
