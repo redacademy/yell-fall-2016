d271236ec64a7202795001f58455114256d916c2
quinlan-alys <shannonqad@gmail.com>
2016-12-10 23:13:43 -0800

    styling people pages for mobile

--- themes/yell/single-people_type.php
+++ themes/yell/single-people_type.php
@@ -7,22 +7,25 @@
 get_header(); ?>
 	<section id="primary" class="content-area-single-people">
 		<main id="main" class="site-main-single-people" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<section class="single-people-container">
+			<!--<?php while ( have_posts() ) : the_post(); ?>-->
+
+			<div class="single-people-container">
+
 				<div class="single-people-thumb">
 					<?php the_post_thumbnail(); ?>
-				</div>
-				<div class="single-people-content">
-					<h2 class="single-people-title">
+						<h2 class="single-people-title">
 						<?php the_title();?>
-					</h2>
+						</h2>
+						<p class="people-position"><?php echo CFS()->get( 'person_position' ); ?></p>
+
 					<p class="single-people-description">
-						<?php echo CFS()->get( 'person_position' ); ?>
-						<?php echo CFS()->get( 'person_description' ); ?>
+						
+						<p><?php echo CFS()->get( 'person_description' ); ?></p>
 					</p>
 				</div>
+				
 			</section>
-				<?php endwhile; // End of the loop. ?>
+				<!--<?php endwhile; // End of the loop. ?>-->
 		</main>
 		<!-- #main -->
 	</section>
