1c251d463f65e2158b6ae583c79b30eaf659b1f1
quinlan-alys <shannonqad@gmail.com>
2016-12-12 16:17:54 -0800

    fixing white blobs

--- themes/yell/404.php
+++ themes/yell/404.php
@@ -11,7 +11,10 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-		<div class="hero-404">404 oops!</div>
+		<div class="hero-404">404 oops!
+			<div class="white-blob">
+				</div>
+		</div>
 			<section class="error-404 not-found">
 				<p> Sorry we’re having trouble finding what you’re looking for.
 Please don’t YELL at us!</p>
