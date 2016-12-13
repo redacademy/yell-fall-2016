9662bcb0d75809897f4271fbb1e5c397e23093f9
Kiana Aghaei <kiana.aghaei@gmail.com>
2016-12-12 17:26:20 -0800

    fixing the way of calling CPT for people

--- themes/yell/page-about.php
+++ themes/yell/page-about.php
@@ -19,12 +19,11 @@ get_header(); ?>
 						<section class="about-text">
 							<div class="about-content-container">
 								<h2>About</h2>
-								
 								<div class="our-mission">
 									<h3>Our Mission</h3>
-								<p>
-									<?php echo CFS()->get( 'our_mission' ); ?>
-								</p>
+									<p>
+										<?php echo CFS()->get( 'our_mission' ); ?>
+									</p>
 								</div>
 								<h3>Our Story</h3>
 								<p>
@@ -32,36 +31,34 @@ get_header(); ?>
 								</p>
 							</div>
 						</section>
-						<h2>Our Team</h2>
-							<div class="people-post">
-								<?php while ( have_posts() ) : the_post(); ?>
-								<?php endwhile; ?>
-								<?php
-								
-								$args = array( 'post_type' => 'people_type', 'posts_per_page' => 10 ); 
-								$loop = new WP_Query( $args ); 
-								
-								while ( $loop->have_posts()) : $loop->the_post(); 
-							
-								echo '<div class="each-person"><div class="post-thumbnail">'; 
-								the_post_thumbnail( $size = 'post-thumbnail', $attr = '');
-								echo'</div>'; 
-								echo '<p class="the-title">';
-								the_title();
-								echo '</p>';?>
+						<h2 class="about-pg-our-team">Our Team</h2>
+						<section class="people-post">
+							<?php
+								$args = array( 'post_type' => 'people_type',
+												'order' => 'ASC',
+												'posts_per_page'=> 6);
+								$posts = get_posts( $args ); // returns an array of posts
+							?>
+								<?php foreach ( $posts as $post ) : setup_postdata( $post ); ?>
+								<div class="each-person">
+									<?php the_post_thumbnail(); ?>
+									<p class="the-title">
+										<?php the_title(); ?>
+									</p>
 									<p class="people-position">
 										<?php echo CFS()->get( 'person_position' ); ?>
 									</p>
-									<a href="<?php echo get_post_type_archive_link( 'single-people_type' ); ?>">READ MORE ></a>
-							</div>
-							<?php endwhile; ?>
-					</div>
+									<a class="about-read-more" href="<?php echo the_permalink();?>">READ MORE ></a>
+								</div>
+					
+					<?php endforeach; wp_reset_postdata(); ?>
+					</section>
 					<div class="aboutpage-button">
 						<h2>Want to learn more?</h2>
-					<div class="about-btns">
-						<a href="<?php echo esc_url( site_url( '/contact/' ) ); ?>" rel="contact"><button class="contact-us"> Contact Us ></button></a>
-						<a href="<?php echo esc_url( site_url( '/program/' ) ); ?>" rel="program"><button class="programs"> Programs ></button></a>
-					</div>
+						<div class="about-btns">
+							<a href="<?php echo esc_url( site_url( '/contact/' ) ); ?>" rel="contact"><button class="contact-us"> Contact Us ></button></a>
+							<a href="<?php echo esc_url( site_url( '/program/' ) ); ?>" rel="program"><button class="programs"> Programs ></button></a>
+						</div>
 					</div>
 				</div>
 			</div>
