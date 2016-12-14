de1e6bebbc908cf5e641f9624c06cd09b42faab6
quinlan-alys <shannonqad@gmail.com>
2016-12-13 20:31:20 -0800

    runthough, minor styling on newsand contact

--- themes/yell/page-contact.php
+++ themes/yell/page-contact.php
@@ -11,9 +11,7 @@
 get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-			<?php get_template_part( 'template-parts/content', 'page' ); ?>
-			<?php endwhile; // End of the loop. ?>
+		
 			<h3>Contact Us</h3>
 			<div class="contactinfo">
 				<div class="contact-profesh">
